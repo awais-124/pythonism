5d7fcf748a16f2aef6b215f457b294921305e788
awais-124 <awais14940@gmail.com>
2024-10-01 11:09:48 +0500

    Added AI folder

---
+++ AI_NLP_ML/tensorflow.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing.text import Tokenizer
+
+sentences = ["I love coding", "I love football"]
+
+tokenizer = Tokenizer(num_words=100)
+tokenizer.fit_on_texts(sentences)
+word_index = tokenizer.word_index
+print(word_index)
